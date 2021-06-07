bab4c7f4a05dfa00fd532d4f035604ef73d93b05
guohb <w769205218@163.com>
2021-06-07 21:14:58 +0800

    测试类添加两个输出v2.0

--- src/test/java/com/ghb/Git2010TestApplicationTests.java
+++ src/test/java/com/ghb/Git2010TestApplicationTests.java
@@ -8,7 +8,7 @@ class Git2010TestApplicationTests {
 
     @Test
     void contextLoads() {
-
+        System.out.println("sdsdsdsdsd");
 
         System.out.println("啦啦啦啦啦啦设立斯蒂芬库里能看见老师的返回那肯定就是你他尽快立法规定和尼克国际对方你看见了海景房的客户九年");
     }
